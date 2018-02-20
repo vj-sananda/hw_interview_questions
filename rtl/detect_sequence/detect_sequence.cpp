51ca546a32538ee2a7864b51a91d1af42c1632f3
Stephen Henry <stephenry@gmail.com>
2018-02-19 20:54:53 -0800

    Updated detect_sequence answer.

--- rtl/detect_sequence/detect_sequence.cpp
+++ rtl/detect_sequence/detect_sequence.cpp
@@ -25,65 +25,69 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //========================================================================== //
 
-#include <libtb.h>
+#include <libtb2.hpp>
 #include "vobj/Vdetect_sequence.h"
 
 #define PORTS(__func)                           \
     __func(in, bool)                            \
     __func(fail_r, bool)
 
-class DetectSequenceTb : libtb::TopLevel
-{
-public:
-    SC_HAS_PROCESS(DetectSequenceTb);
-    DetectSequenceTb(sc_core::sc_module_name mn = "t")
-        : uut_("uut")
-#define __declare_signals(__name, __type)       \
-        , __name##_(#__name)
-        PORTS(__declare_signals)
-#undef __declare_signals
-    {
-        SC_METHOD(m_checker);
-        dont_initialize();
-        sensitive << e_tb_sample();
+typedef Vdetect_sequence uut_t;
 
-        uut_.clk(clk());
-        uut_.rst(rst());
-#define __bind_signals(__name, __type)       \
-        uut_.__name(__name##_);
-        PORTS(__bind_signals)
+struct DetectSequenceTb : libtb2::Top<DetectSequenceTb> {
+  SC_HAS_PROCESS(DetectSequenceTb);
+  DetectSequenceTb(sc_core::sc_module_name mn = "t")
+    : uut_("uut") {
+    //
+    resetter_.clk(clk_);
+    resetter_.rst(rst_);
+    //
+    sampler_.clk(clk_);
+    //
+    wd_.clk(clk_);
+    //
+    uut_.clk(clk_);
+    uut_.rst(rst_);
+#define __bind_signals(__name, __type)          \
+    uut_.__name(__name ## _);
+    PORTS(__bind_signals)
 #undef __bind_signals
 
-            wave_on("foo.vcd", uut_);
-    }
+    SC_METHOD(m_checker);
+    sensitive << sampler_.sample();
+    dont_initialize();
+    SC_THREAD(t_stimulus);
+  }
+private:
+  void t_stimulus() {
+    scv_smart_ptr<bool> in;
 
-    bool run_test()
-    {
-        LIBTB_REPORT_INFO("Stimulus starts....");
-        for (int i = 0; i < N; i++) {
-            in_ = (libtb::random_integer_in_range(100) < 40);
-            t_wait_posedge_clk();
-        }
-        LIBTB_REPORT_INFO("Stimulus ends.");
-        return false;
-    }
+    wait(resetter_.done());
+    while (true) {
+      in->next();
 
-    void m_checker() {
-        LIBTB_ASSERT_ERROR(!fail_r_);
+      in_ = *in;
+      LOGGER(INFO) << "in = " << in_ << "\n";
+      wait(clk_.posedge_event());
     }
-
-    const int N{10000};
+  }
+  void m_checker() {
+    LIBTB2_ERROR_ON(fail_);
+  }
+  sc_core::sc_clock clk_;
+  sc_core::sc_signal<bool> rst_;
 #define __declare_signals(__name, __type)       \
-    sc_core::sc_signal<__type> __name##_;
-    PORTS(__declare_signals)
+  sc_core::sc_signal<__type> __name##_;
+  PORTS(__declare_signals)
 #undef __declare_signals
-    Vdetect_sequence uut_;
+  libtb2::Sampler sampler_;
+  libtb2::Resetter resetter_;
+  libtb2::SimWatchDogCycles wd_;
+  uut_t uut_;
 };
+SC_MODULE_EXPORT(DetectSequenceTb);
 
-int sc_main (int argc, char **argv)
-{
-    using namespace libtb;
-    DetectSequenceTb t;
-    LibTbContext::init(argc, argv);
-    return LibTbContext::start();
+int sc_main(int argc, char ** argv) {
+  DetectSequenceTb tb;
+  return libtb2::Sim::start(argc, argv);
 }
