d52f8a0d9e5509ff8c510669d9300bae307dd974
Stephen Henry <stephenry@gmail.com>
2018-02-21 20:28:57 -0800

    Updated fifo_n solution.

--- rtl/fifo_n/fifo_n.cpp
+++ rtl/fifo_n/fifo_n.cpp
@@ -25,162 +25,164 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //========================================================================== //
 
-#include <libtb.h>
+#include <libtb2.hpp>
 #include <deque>
-#include <sstream>
 #include "vobj/Vfifo_n.h"
 
 #define PORTS(__func)                           \
   __func(push, bool)                            \
-  __func(push_vq, VqEncT)                       \
-  __func(push_data, WordT)                      \
+  __func(push_vq, uint32_t)                     \
+  __func(push_data, uint32_t)                   \
   __func(pop, bool)                             \
-  __func(pop_vq, VqEncT)                        \
+  __func(pop_vq, uint32_t)                      \
   __func(pop_data_valid_r, bool)                \
-  __func(pop_data_vq_r, VqEncT)                 \
-  __func(pop_data_w, WordT)                     \
-  __func(empty_r, VqT)                          \
-  __func(full_r, VqT)
+  __func(pop_data_vq_r, uint32_t)               \
+  __func(pop_data_w, uint32_t)                  \
+  __func(empty_r, uint32_t)                     \
+  __func(full_r, uint32_t)
 
-constexpr int OPT_VQ_N = 8;
-constexpr int OPT_N = 8;
+const std::size_t N = 8;
+const std::size_t VQ_N = 8;
 
-struct FifoNTb : libtb::TopLevel
-{
-  using UUT = Vfifo_n;
-
-  using VqEncT = uint32_t;
-  using WordT = uint32_t;
-  using VqT = uint32_t;
+struct FifoNTb : libtb2::Top<FifoNTb> {
+  typedef Vfifo_n uut_t;
   
   SC_HAS_PROCESS(FifoNTb);
   FifoNTb(sc_core::sc_module_name mn = "t")
-    : uut_("uut")
-#define __construct_signals(__name, __type)     \
-    , __name##_(#__name)
-    PORTS(__construct_signals)
-#undef __construct_signals
-  {
+    : uut_("uut") {
+    //
+    resetter_.clk(clk_);
+    resetter_.rst(rst_);
+    //
+    sampler_.clk(clk_);
+    //
+    wd_.clk(clk_);
+    //
+    uut_.clk(clk_);
+    uut_.rst(rst_);
+#define __bind_ports(__name, __type)            \
+    uut_.__name(__name ## _);
+    PORTS(__bind_ports)
+#undef __bind_ports
+    SC_THREAD(t_push);
+    SC_THREAD(t_pop);
     SC_METHOD(m_checker);
+    sensitive << sampler_.sample();
     dont_initialize();
-    sensitive << e_tb_sample();
-
-    SC_THREAD(t_popper);
-              
-    uut_.clk(clk());
-    uut_.rst(rst());
-#define __bind_signals(__name, __type)          \
-    uut_.__name(__name##_);
-    PORTS(__bind_signals)
-#undef __bind_signals
   }
+private:
+  void m_checker() {
+    if (!pop_data_valid_r_)
+      return ;
 
-  void b_push_idle()
-  {
-    push_ = false;
-    push_vq_ = VqT();
-    push_data_ = WordT();
+    LIBTB2_ERROR_ON(vq_.empty());
+    const std::size_t vq = vq_.front(); vq_.pop_front();
+    const uint32_t expected = expected_[vq].front(); expected_[vq].pop_front();
+    const uint32_t actual = pop_data_w_;
+    LOGGER(INFO) << "Pop vq = " << vq
+                 << " actual = " << std::hex << actual
+                 << " expected = " << std::hex << expected
+                 << "\n";
+    LIBTB2_ERROR_ON(actual != expected);
   }
-
-  void b_push(VqT vq)
-  {
-    const WordT w = libtb::random<WordT>();
-    push_ = true;
-    push_vq_ = vq;
-    push_data_ = w;
-    t_wait_posedge_clk();
-    fifo_n_[vq].push_back(w);
-    b_push_idle();
-  }
-
-  bool run_test()
-  {
-    LIBTB_REPORT_INFO("Stimulus starts");
-    int n = N;
-    while (n--) {
-      const int vq = libtb::random_integer_in_range(OPT_VQ_N - 1);
-
-      t_wait_sync();
-      if (fifo_n_[vq].size() == OPT_N) {
-        LIBTB_ASSERT_ERROR(full_r_ | (1 << vq));
-        t_wait_posedge_clk();
-      }
-      else
-        b_push(vq);
-    }
-    
-    LIBTB_REPORT_INFO("Stimulus ends");
-    return true;
-  }
-
-  void b_pop_idle()
-  {
+  void t_pop() {
     pop_ = false;
-    pop_vq_ = VqT(0);
-  }
+    wait(resetter_.done());
+    scv_smart_ptr<bool> ppop;
+    scv_bag<bool> pop_bag;
+    pop_bag.add(true, 10);
+    pop_bag.add(false, 90);
+    ppop->set_mode(pop_bag);
 
-  void b_pop (VqT vq)
-  {
-    pop_ = true;
-    pop_vq_ = vq;
-    const WordT w = fifo_n_[vq].front();
-    fifo_n_[vq].pop_front();
-    expectation_.push_back(w);
-    t_wait_posedge_clk(1);
-    b_pop_idle();
-  }
-
-  void t_popper()
-  {
-    t_wait_reset_done();
-
-    LIBTB_REPORT_DEBUG("Popper thread begins.");
-    
     while (true) {
-      t_wait_sync();
-      const int vq = libtb::random_integer_in_range(OPT_VQ_N-1);
-      if (fifo_n_[vq].size() > 0) {
-        LIBTB_ASSERT_ERROR(~(empty_r_ | (1 << vq)));
-        b_pop(vq);
-      } else
-        t_wait_posedge_clk(1);
+      wait(clk_.posedge_event());
+      pop_ = false;
+      
+      ppop->next();
+
+      const bool pop = *ppop;
+      if (!pop)
+        continue;
+
+      const uint32_t empty_r = libtb2::mask_bits<uint32_t>(empty_r_, VQ_N);
+      if (libtb2::mask_bits(empty_r, VQ_N) == libtb2::ones<uint32_t>(VQ_N))
+        continue;
+
+      uint32_t pop_vq = libtb2::pickone(
+          libtb2::mask_bits<uint32_t>(~empty_r, VQ_N));
+      LIBTB2_ASSERT(pop_vq < VQ_N);
+      LIBTB2_ASSERT(pop_vq != 0);
+      pop_vq -= 1;
+      
+      pop_ = true;
+      pop_vq_ = pop_vq;
+
+      // RTL reports empty, however predicted model does not.
+      LIBTB2_ERROR_ON(expected_[pop_vq].empty());
+      vq_.push_back(pop_vq);
     }
   }
+  void t_push() {
+    push_ = false;
+    
+    scv_smart_ptr<bool> ppush;
+    scv_smart_ptr<uint32_t> ppush_data;
+    scv_bag<bool> push_bag;
+    push_bag.add(true, 30);
+    push_bag.add(false, 70);
+    ppush->set_mode(push_bag);
+    
+    wait(resetter_.done());
+    while (true) {
+      wait(clk_.posedge_event());
+      push_ = false;
+      
+      ppush->next();
+      ppush_data->next();
+      
+      const bool push = *ppush;
+      const uint32_t push_data = *ppush_data;
+      wait(sampler_.sample());
+      const uint32_t full = libtb2::mask_bits<uint32_t>(full_r_, VQ_N);
 
-  void m_checker()
-  {
+      if (!push)
+        continue;
 
-    // Check output
-    if (pop_data_valid_r_) {
+      if (libtb2::mask_bits(full, VQ_N) == libtb2::ones<uint32_t>(VQ_N))
+        continue;
 
-      if (expectation_.size() == 0) {
-        LIBTB_REPORT_ERROR("Unexpected completion.");
-        return ;
-      }
+      push_ = true;
+      push_data_ = push_data;
+      uint32_t push_vq = libtb2::pickone(libtb2::mask_bits(~full, VQ_N));
+      LIBTB2_ASSERT(push_vq != 0);
+      push_vq -= 1;
+      
+      push_vq_ = push_vq;
 
-      const WordT expected = expectation_.front();
-      expectation_.pop_front();
-      const WordT actual = pop_data_w_;
-      if (expected != actual) {
-        std::stringstream ss;
-        ss << "Mismatch detected";
-        LIBTB_REPORT_ERROR(ss.str());
-      }
+      // RTL does not indicate ful however predicted model does.
+      LIBTB2_ERROR_ON(expected_[push_vq].size() == N);
+
+      LOGGER(INFO) << "Push vq = " << push_vq
+                   << " data = " << std::hex << push_data << "\n";
+      expected_[push_vq].push_back(push_data);
     }
   }
-
-  const int N{10000};
-  std::deque<WordT> fifo_n_[OPT_VQ_N];
-  std::deque<WordT> expectation_;
-#define __declare_signals(__name, __type)       \
+  std::deque<uint32_t> expected_ [N];
+  std::deque<std::size_t> vq_;
+  sc_core::sc_clock clk_;
+  sc_core::sc_signal<bool> rst_;
+#define __declare_signal(__name, __type)        \
   sc_core::sc_signal<__type> __name##_;
-  PORTS(__declare_signals)
-#undef __declare_signals
-  UUT uut_;
+  PORTS(__declare_signal)
+#undef __declare_signal
+  libtb2::Resetter resetter_;
+  libtb2::Sampler sampler_;
+  libtb2::SimWatchDogCycles wd_;
+  uut_t uut_;
 };
+SC_MODULE_EXPORT(FifoNTb<uint32_t>);
 
-int sc_main (int argc, char **argv)
-{
-  using namespace libtb;
-  return LibTbSim<FifoNTb>(argc, argv).start();
+int sc_main(int argc, char **argv) {
+  FifoNTb tb;
+  return libtb2::Sim::start(argc, argv);
 }
