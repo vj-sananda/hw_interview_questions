4160a73ade4f200c7d8dc34b928e2edff445caa1
Stephen Henry <stephenry@gmail.com>
2018-03-12 19:56:52 +0000

    Updated zero_indices_fast.

--- rtl/zero_indices_fast/zero_indices_fast.cpp
+++ rtl/zero_indices_fast/zero_indices_fast.cpp
@@ -25,143 +25,136 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //========================================================================== //
 
-#include <libtb.h>
-#include <deque>
-#include <sstream>
+#include <libtb2.hpp>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 #include <bitset>
-
 #include "vobj/Vzero_indices_fast.h"
-
-#define ZERO_PROBABILITY 20
+typedef Vzero_indices_fast uut_t;
 
 #define PORTS(__func)                           \
-  __func(in_vector, sc_dt::sc_bv<128>)          \
+  __func(in_vector, sc_bv<128>)                 \
   __func(in_start, bool)                        \
   __func(in_busy_r, bool)                       \
   __func(resp_valid_r, bool)                    \
   __func(resp_index_r, uint32_t)
 
+typedef Vzero_indices_fast uut_t;
 
-struct ZeroIndicesFastTb : libtb::TopLevel
-{
-  using VectorT = sc_dt::sc_bv<128>;
-  
-  struct Stimulus
-  {
-    VectorT v;
-    std::deque<uint32_t> p;
-  };
-  
-  using RespQueueT = std::deque<Stimulus>;
-  
-  using UUT_t = Vzero_indices_fast;
-  SC_HAS_PROCESS(ZeroIndicesFastTb);
-  ZeroIndicesFastTb(sc_core::sc_module_name mn =  "t")
-    : uut_("uut")
-#define __construct_signal(__name, __type)      \
-    , __name##_(#__name)
-    PORTS(__construct_signal)
-#undef __construct_signal
-  {
-    SC_METHOD(m_checker);
-    dont_initialize();
-    sensitive << e_tb_sample();
-    
-    uut_.clk(clk());
-    uut_.rst(rst());
-#define __bind_signal(__name, __type)           \
-    uut_.__name(__name##_);
-    PORTS(__bind_signal)
-#undef __bind_signals
-  }
-
-  Stimulus construct_stimulus()
-  {
-    Stimulus s;
-    for (int i = 0; i < 128; i++)
-      s.v[i] = (libtb::random_integer_in_range(100) < (100 - ZERO_PROBABILITY));
-
-    VectorT v = s.v;
-    for (int i = 0; i < 128; i++) {
-      if (!v[i])
-        s.p.push_back(i);
-    }
-    return s;
-  }
-
-  void b_issue_in(const Stimulus & s)
-  {
-    std::stringstream ss;
-    ss << "Issuing vector: " << s.v.to_string(SC_BIN);
-    LIBTB_REPORT_DEBUG(ss.str());
-
-    in_start_ = true;
-    in_vector_ = s.v;
-    t_wait_posedge_clk(1);
-    in_start_ = false;
-    wait(in_busy_r_.negedge_event());
-  }
-
-  bool run_test() {
-    t_wait_reset_done();
-    LIBTB_REPORT_INFO("Stimulus starts...");
-
-    for (int i = 0; i < 10000; i++) {
-      const Stimulus c = construct_stimulus();
-      q_.push_back(c);
-      b_issue_in(c);
-    }
-    
-    LIBTB_REPORT_INFO("Stimulus ends..");
-    return false;
-  }
-
-  void m_checker()
-  {
-    if (resp_valid_r_) {
-
-    __retry:
-      if (q_.size() == 0) {
-        std::stringstream ss;
-        ss << "Unexpected response";
-        LIBTB_REPORT_FATAL(ss.str());
-        return;
-      }
-
-      Stimulus & s = q_.front();
-      if (s.p.size() == 0) {
-        q_.pop_front();
-        goto __retry;
-      }
-
-      const uint32_t expected = s.p.front(); s.p.pop_front();
-      const uint32_t actual = resp_index_r_;
-
-      if (expected != actual) {
-        std::stringstream ss;
-        ss << "Mismatch "
-           << " Actual:" << std::dec << actual
-           << " Expected:" << std::dec << expected
-          ;
-        LIBTB_REPORT_ERROR(ss.str());
-      } else {
-        std::stringstream ss;
-        ss << "Validated index: " << std::dec << actual;
-        LIBTB_REPORT_DEBUG(ss.str());
-      }
-    }
-  }
-
-  RespQueueT q_{};
-#define __declare_signal(__name, __type)        \
-  sc_core::sc_signal<__type> __name##_;
-  PORTS(__declare_signal)
-#undef __declare_signal
-  UUT_t uut_;
+struct ZeroIndicesXActIntf : sc_core::sc_interface {
+  virtual void issue(const sc_bv<128> & x) = 0;
 };
 
-int sc_main(int argc, char **argv)
-{
-  using namespace libtb;
-  return LibTbSim<ZeroIndicesFastTb>(argc, argv).start();
+struct ZeroIndicesXAct : ZeroIndicesXActIntf, sc_core::sc_module {
+  sc_core::sc_in<bool> clk;
+  sc_core::sc_out<sc_bv<128> > in_vector;
+  sc_core::sc_out<bool> in_start;
+  sc_core::sc_in<bool> in_busy_r;
+  ZeroIndicesXAct(sc_core::sc_module_name mn = "xact")
+    : sc_core::sc_module(mn)
+    , clk("clk")
+    , in_vector("in_vector")
+    , in_start("in_start")
+    , in_busy_r("in_busy_r")
+  {}
+  void issue(const sc_bv<128> & x) {
+    in_start = true;
+    in_vector = x;
+    wait(clk.posedge_event());
+    idle();
+  }
+private:
+  void idle() {
+    in_start = false;
+    in_vector = 0;
+  }
+};
+
+struct ZeroIndicesFastTb : libtb2::Top<ZeroIndicesFastTb> {
+  sc_core::sc_port<ZeroIndicesXActIntf> p;
+  SC_HAS_PROCESS(ZeroIndicesFastTb);
+  ZeroIndicesFastTb(sc_core::sc_module_name mn = "t")
+    : uut_("uut"), p("p"), xact_("xact")
+  {
+    //
+    resetter_.clk(clk_);
+    resetter_.rst(rst_);
+    //
+    p.bind(xact_);
+    xact_.clk(clk_);
+    xact_.in_vector(in_vector_);
+    xact_.in_start(in_start_);
+    xact_.in_busy_r(in_busy_r_);
+    //
+    wd_.clk(clk_);
+    sampler_.clk(clk_);
+    //
+    uut_.clk(clk_);
+    uut_.rst(rst_);
+#define __bind_ports(__name, __type)            \
+    uut_.__name(__name ## _);
+    PORTS(__bind_ports)
+#undef __bind_ports
+
+    SC_THREAD(t_stimulus);
+    SC_METHOD(m_checker);
+    sensitive << sampler_.sample();
+    dont_initialize();
+  }
+private:
+  void t_stimulus() {
+    resetter_.wait_reset_done();
+
+    scv_smart_ptr<sc_bv<128> > in_vector;
+    while (true) {
+
+      in_vector->next();
+
+      const sc_bv<128> actual = *in_vector;
+      LOGGER(INFO) << "Issue new word = "
+                   << actual
+                   << " zero count = " << libtb2::popcount(~actual)
+                   << "\n";
+      zero_indices_.clear();
+      libtb2::find_bits2(actual, std::back_inserter(zero_indices_));
+      std::reverse(zero_indices_.begin(), zero_indices_.end());
+      p->issue(actual);
+
+      if (!in_busy_r_)
+        wait(in_busy_r_.posedge_event());
+
+      while (in_busy_r_)
+        wait(clk_.posedge_event());
+    }
+  }
+  void m_checker() {
+    if (resp_valid_r_) {
+      LIBTB2_ERROR_ON(zero_indices_.size() == 0);
+
+      const uint32_t actual = resp_index_r_;
+      const uint32_t expected = zero_indices_.back(); zero_indices_.pop_back();
+
+      LIBTB2_ERROR_ON(actual != expected);
+      LOGGER(INFO) << "\tIndex = " << actual << " (" << expected << ")\n";
+    }
+  }
+  std::vector<std::size_t> zero_indices_;
+  ZeroIndicesXAct xact_;
+  sc_core::sc_clock clk_;
+  sc_core::sc_signal<bool> rst_;
+#define __declare_signals(__name, __type)       \
+  sc_core::sc_signal<__type> __name ## _;
+  PORTS(__declare_signals)
+#undef __declare_signals
+  libtb2::Resetter resetter_;
+  libtb2::Sampler sampler_;
+  libtb2::SimWatchDogCycles wd_;
+  uut_t uut_;
+};
+SC_MODULE_EXPORT(ZeroIndicesFastTb);
+
+int sc_main(int argc, char ** argv) {
+  ZeroIndicesFastTb tb;
+  return libtb2::Sim::start(argc, argv);
 }
