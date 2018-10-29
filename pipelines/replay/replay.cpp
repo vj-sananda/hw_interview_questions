dbae3bbf0940e3364aba9c3a9058913f8ea2956b
Stephen Henry <stephenry@gmail.com>
2018-10-28 21:46:56 -0700

    Addition of replay solution.

---
+++ pipelines/replay/replay.cpp
@@ -0,0 +1,177 @@
+//========================================================================== //
+// Copyright (c) 2018, Stephen Henry
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//========================================================================== //
+
+#include <libtb2.hpp>
+#include <vector>
+#include <iostream>
+
+#include "vobj/Vreplay.h"
+
+#define PORTS(__func)                           \
+  __func(in, uint32_t)                          \
+  __func(in_vld, bool)                          \
+  __func(in_accept, bool)                       \
+  __func(out_r, uint32_t)                       \
+  __func(out_vld_r, bool)                       \
+  __func(replay_s8_w, bool)                     \
+  __func(stall_req, uint32_t)
+
+typedef Vreplay uut_t;
+
+struct CombStallTb : libtb2::Top<uut_t> {
+  SC_HAS_PROCESS(CombStallTb);
+  CombStallTb(sc_core::sc_module_name mn = "t")
+    : uut_("uut")
+#define __construct_ports(__name, __type)       \
+      , __name ## _(#__name)
+    PORTS(__construct_ports)
+#undef __construct_ports
+    , clk_("clk")
+    , rst_("rst")
+  {
+    uut_.clk(clk_);
+    uut_.rst(rst_);
+#define __bind_signals(__name, __type)          \
+    uut_.__name(__name ## _);
+    PORTS(__bind_signals)
+#undef __bind_signals
+
+    resetter_.clk(clk_);
+    resetter_.rst(rst_);
+    
+    sampler_.clk(clk_);
+
+    generate_stimulus(1024);
+
+    SC_THREAD(t_in);
+    
+    SC_METHOD(m_out);
+    sensitive << sampler_.sample();
+    dont_initialize();
+
+    stall_n_ = 0;
+    SC_METHOD(m_stall);
+    sensitive << clk_.posedge_event();
+
+    SC_THREAD(t_replay);
+
+    register_uut(uut_);
+    vcd_on();
+  }
+private:
+  void t_replay() {
+    scv_smart_ptr<bool> replay;
+    scv_bag<bool> replay_bag;
+    replay_bag.add(true, 1);
+    replay_bag.add(false, 99);
+    replay->set_mode(replay_bag);
+    
+    replay_s8_w_ = false;
+    while (true) {
+      replay->next();
+
+      replay_s8_w_ = *replay;
+      wait(clk_.posedge_event());
+      replay_s8_w_ = false;
+    }
+  }
+  void m_stall() {
+    if ((++stall_n_ % 10) == 0) {
+      stall_req_ = (1 << (*stall_ptr_ % 10));
+
+      stall_ptr_->next();
+    } else {
+      stall_req_ = 0;
+    }
+  }
+  void generate_stimulus(std::size_t n = 10) {
+    scv_smart_ptr<uint32_t> rnd;
+    stimulus_.clear();
+    i_ = 0;
+    j_ = 0;
+    while (n-- != 0) {
+      rnd->next();
+      stimulus_.push_back(*rnd);
+    }
+  }
+  void t_in() {
+    in_vld_ = false;
+    in_ = 0;
+    
+    resetter_.wait_reset_done();
+
+    wait(10, SC_NS);
+
+    while (i_ < stimulus_.size()) {
+      wait(clk_.posedge_event());
+
+      in_vld_ = true;
+      in_ = stimulus_[i_];
+      wait(sampler_.sample());
+      if (in_accept_)
+        i_++;
+    }
+    in_vld_ = false;
+
+    wait(100, SC_NS);
+    sc_core::sc_stop();
+  }
+  void m_out() {
+    if (out_vld_r_) {
+      const uint32_t actual = out_r_;
+      const uint32_t expected = stimulus_[j_++];
+
+      std::cout << "[" << sc_core::sc_time_stamp() << "] ";
+      if (actual != expected)
+        std::cout << "*** Mismatch expected = " << expected
+                  << " actual = " << actual;
+      else
+        std::cout << "Match!";
+
+      std::cout << "\n";
+    }
+  }
+  libtb2::Resetter resetter_;
+  libtb2::Sampler sampler_;
+  std::vector<uint32_t> stimulus_;
+  std::size_t i_, j_;
+  sc_core::sc_clock clk_;
+  sc_core::sc_signal<bool> rst_;
+#define __declare_signal(__name, __type)        \
+  sc_core::sc_signal<__type> __name##_;
+  PORTS(__declare_signal)
+#undef __declare_signal
+  std::size_t stall_n_;
+  scv_smart_ptr<uint32_t> stall_ptr_;
+  uut_t uut_;
+};
+SC_MODULE_EXPORT(CombStallTb);
+
+int sc_main(int argc, char **argv) {
+  CombStallTb tb("tb");
+  return libtb2::Sim::start(argc, argv);
+}
---
+++ pipelines/replay/replay.sv
@@ -0,0 +1,287 @@
+//========================================================================== //
+// Copyright (c) 2018, Stephen Henry
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//========================================================================== //
+
+`include "libtb2.vh"
+`include "libv2_pkg.vh"
+
+module replay #(parameter int N = 10, parameter int W = 32) (
+
+   //======================================================================== //
+   //                                                                         //
+   // Misc.                                                                   //
+   //                                                                         //
+   //======================================================================== //
+
+     input                                        clk
+   , input                                        rst
+
+   //======================================================================== //
+   //                                                                         //
+   // In                                                                      //
+   //                                                                         //
+   //======================================================================== //
+
+   , input          [W-1:0]                       in
+   , input                                        in_vld
+   //
+   , output logic                                 in_accept
+
+   //======================================================================== //
+   //                                                                         //
+   // Out                                                                     //
+   //                                                                         //
+   //======================================================================== //
+
+   , output logic    [W-1:0]                      out_r
+   , output                                       out_vld_r
+
+   //======================================================================== //
+   //                                                                         //
+   // Cntrl                                                                   //
+   //                                                                         //
+   //======================================================================== //
+
+   , input  logic                                 replay_s8_w
+   , input  logic   [N-1:0]                       stall_req
+);
+
+  typedef struct packed {
+    logic        o;
+    logic [2:0]  p;
+  } ptr_t;
+
+  //
+  logic [N-1:1]                         vld_r;
+  logic [N-1:1]                         vld_w;
+
+  //
+  logic [N-1:1][W-1:0]                  ucode_r;
+  logic [N-1:1][W-1:0]                  ucode_w;
+
+  //
+  logic [N-1:0]                         adv;
+  logic [N-1:1]                         en;
+  logic [N-1:0]                         stall;
+  logic [N-1:0]                         kill;
+
+  //
+  logic                                 commit_s8;
+  logic                                 replay_s8_r;
+
+  //
+  ptr_t                                 rd_ptr_arch_r;
+  ptr_t                                 rd_ptr_arch_w;
+  logic                                 rd_ptr_arch_en;
+  //
+  ptr_t                                 rd_ptr_spec_r;
+  ptr_t                                 rd_ptr_spec_w;
+  logic                                 rd_ptr_spec_en;
+  //
+  ptr_t                                 wr_ptr_r;
+  ptr_t                                 wr_ptr_w;
+  logic                                 wr_ptr_en;
+  //
+  logic                                 empty_w;
+  logic                                 empty_r;
+  logic                                 full_w;
+  logic                                 full_r;
+  //
+  logic [7:0][W-1:0]                    fifo_r;
+  logic                                 fifo_en;
+
+  // ======================================================================== //
+  //                                                                          //
+  // Combinatorial Logic                                                      //
+  //                                                                          //
+  // ======================================================================== //
+  
+  // ------------------------------------------------------------------------ //
+  //
+  always_comb
+    begin : front_end_PROC
+
+      //
+      rd_ptr_arch_en = commit_s8;
+      rd_ptr_arch_w  = commit_s8 ? (rd_ptr_arch_r + 'b1) : rd_ptr_arch_r;
+
+      //
+      rd_ptr_spec_en = (replay_s8_r | adv [0]);
+
+      //
+      casez ({replay_s8_r, adv [0]})
+        2'b1?:   rd_ptr_spec_w  = rd_ptr_arch_r;
+        2'b01:   rd_ptr_spec_w  = rd_ptr_spec_r + 'b1;
+        default: rd_ptr_spec_w  = rd_ptr_spec_r;
+      endcase // casez ({replay_s8_r, adv [0]})
+      
+      //
+      fifo_en   = in_vld & in_accept;
+
+      //
+      wr_ptr_en = fifo_en;
+      wr_ptr_w  = (fifo_en) ? wr_ptr_r + 'b1 : wr_ptr_r;
+
+      //
+      empty_w  = (rd_ptr_spec_w == wr_ptr_w);
+      full_w   = (rd_ptr_arch_w.o  ^ wr_ptr_w.o) &
+                 (rd_ptr_arch_w.p == wr_ptr_w.p);
+
+    end // block: front_end_PROC
+  
+  // ------------------------------------------------------------------------ //
+  //
+  always_comb
+    begin : pipe_PROC
+
+      // The final (output) stage cannot be stalled (for DV simplicity).
+      //
+      stall [N - 1]  = 1'b0;
+
+      // Kill stages on upstream replay.
+      //
+      kill           = '0;
+      for (int i = 0; i < N; i++)
+        kill [i]  = (i <= 8) ? replay_s8_r : '0;
+
+      //
+      for (int i = N - 2; i >= 0; i--)
+        stall [i]  = ((i == 0) ? 1'b1 :vld_r [i]) & (stall_req [i] | stall [i + 1]);
+      
+      //
+      in_accept    = (~full_r);
+
+      //
+      for (int i = 0; i < N; i++)
+        adv [i]  = ((i == 0) ? (~empty_r) : vld_r [i]) & (~stall [i]) & (~kill [i]);
+                  
+      //
+      for (int i = 1; i < N; i++)
+        en [i]    = adv [i - 1];
+      
+      //
+      for (int i = 1; i < N; i++) begin
+        
+        casez ({kill [i], stall [i]})
+          2'b1?:   vld_w [i]  = 'b0;
+          2'b00:   vld_w [i]  = adv [i - 1];
+          default: vld_w [i]  = vld_r [i];
+        endcase // casez ({kill [i], stall [i]})
+
+      end // for (int i = 1; i < N; i++)
+                    
+      //
+      for (int i = 1; i < N; i++)
+        ucode_w [i]  = (i == 1) ? fifo_r [rd_ptr_spec_r] : ucode_r [i - 1];
+
+      // Commit point at stage 8. 
+      //
+      commit_s8      = (~replay_s8_r) & (adv [8]);
+                 
+      //
+      out_r          = ucode_r [N - 1];
+      out_vld_r      = vld_r [N - 1];
+
+    end // block: pipe_PROC
+  
+  // ======================================================================== //
+  //                                                                          //
+  // Sequential Logic                                                         //
+  //                                                                          //
+  // ======================================================================== //
+
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (rst)
+      vld_r <= 'b0;
+    else begin
+      for (int i = 1; i < N; i++)
+        vld_r [i] <= vld_w [i];
+    end
+  
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    for (int i = 1; i < N; i++)
+      if (en [i])
+        ucode_r [i] <= ucode_w [i];
+
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (rst)
+      replay_s8_r <= '0;
+    else
+      replay_s8_r <= replay_s8_w;
+  
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (rst) 
+      rd_ptr_arch_r <= '0;
+    else if (rd_ptr_arch_en)
+      rd_ptr_arch_r <= rd_ptr_arch_w;
+  
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (rst) 
+      rd_ptr_spec_r <= '0;
+    else if (rd_ptr_spec_en)
+      rd_ptr_spec_r <= rd_ptr_spec_w;
+  
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (rst) 
+      wr_ptr_r <= '0;
+    else if (wr_ptr_en)
+      wr_ptr_r <= wr_ptr_w;
+  
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (fifo_en)
+      fifo_r [wr_ptr_r] <= in;
+
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (rst)
+      full_r <= 'b0;
+    else
+      full_r <= full_w;
+
+  // ------------------------------------------------------------------------ //
+  //
+  always_ff @(posedge clk)
+    if (rst)
+      empty_r <= 'b1;
+    else
+      empty_r <= empty_w;
+  
+endmodule // replay
